5fcfa2ab0dd30f9088ceaf603180a7a3f42d4dd9
praveena-p25 <80034428+praveena-p25@users.noreply.github.com>
2021-03-10 20:20:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { Table } from './Table';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Payments</h1>
+     <Table/>
+    </div>
+  );
+}
+
+export default App;
